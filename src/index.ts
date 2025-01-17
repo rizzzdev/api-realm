e48dd2c685d87c456d88abeb1eec0ddcd8cc9723
Rizzz Maooo <rizqonmaulana05@gmail.com>
2025-01-17 22:15:33 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import routes from "./routes";
 import ENV from "./configs/env.config";
 import cookieParser from "cookie-parser";
 import logger from "./utils/logger.util";
+import cors from "cors";
 import qs from "qs";
 
 const app: Application = express();
@@ -13,14 +14,14 @@ app.set("query parser", (str: string) => qs.parse(str));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: ENV.CLIENT_URL,
-//     methods: ["GET", "POST", "PATCH"],
-//     allowedHeaders: ["Content-Type", "Authorization"]
-//   })
-// );
+app.use(
+  cors({
+    credentials: true,
+    origin: ENV.CLIENT_URL,
+    methods: ["GET", "POST", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
 routes(app);
 app.use("/v1/coba", (req, res) => {
   res.send(req.headers.authorization);
